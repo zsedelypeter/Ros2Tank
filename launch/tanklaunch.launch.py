1d2abbb63f7a8597e1b7d8115ef19e6d592b2e77
zsedelypeter <zsedely.peter@gmail.com>
2024-12-20 15:09:41 +0100

    Rviz now displays the lidar, added correct rviz config

--- launch/tanklaunch.launch.py
+++ launch/tanklaunch.launch.py
@@ -2,9 +2,12 @@ from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
-    tank_sdf='/home/ajr/ros2_ws/src/ros2tank/simulation/tank.sdf'
+    tank_sdf = '/home/ajr/ros2_ws/src/ros2tank/simulation/tank.sdf'
+    rviz_config_dir = os.path.join(get_package_share_directory('ros2tank'), '')
+    rviz_config_file = os.path.join(rviz_config_dir, 'rvizconfig.rviz')
 
 
     print(f"Looking for tank.sdf at: {tank_sdf}")
@@ -14,7 +17,39 @@ def generate_launch_description():
         output='screen'
     )
 
+    # Configure RViz
+    package_name = 'ros2tank'  # replace with your actual package name
+    rviz_config_dir = os.path.join(get_package_share_directory(package_name), 'rviz')
+    rviz_config_file = os.path.join(rviz_config_dir, 'tank_lidar.rviz')
+
+    rviz_node = Node(
+    package='rviz2',
+    executable='rviz2',
+    name='rviz2',
+    arguments=['-d', rviz_config_file],
+    output='screen'
+)
+
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/lidar/points@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloudPacked'],
+        output='screen'
+    )
+
+# Launch tf tree
+    tf_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_transform_publisher',
+        arguments=['0', '0', '0', '0', '0', '0', '1', 'tank/chassis/gpu_lidar', 'base_link'],
+        output='screen'
+    )
+
     ld = LaunchDescription()
     ld.add_action(gazebo)
+    ld.add_action(rviz_node)
+    ld.add_action(bridge)
+    ld.add_action(tf_node)
 
     return ld
